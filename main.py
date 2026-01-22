4813f5aee6a9bc5e87ff9298dbd5e7e461974582
saimon-dahal <saimondahal66@gmail.com>
2026-01-22 23:16:07 +1100

    feat: add menu bar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from ui_components import MenuBar
 
 class ImageEditorApp:
     def __init__(self, root):
@@ -6,6 +7,20 @@ class ImageEditorApp:
         self.root.title("SYDN-17 Image Editor")
         self.root.geometry("1000x700")
 
+        self.setup_ui()
+    
+    def setup_ui(self):
+        self.menu = MenuBar(self.root, {
+            'open': self.todo,
+            'save': self.todo,
+            'save_as': self.todo,
+            'undo': self.todo,
+            'redo': self.todo,
+            'about': self.todo
+        })
+    
+    def todo(self):
+        print("Placeholder for unwritten features...")
 
 if __name__ == "__main__":
     root = tk.Tk()
