0c1f7ea9040b28485e7c5e847de8751c0a327a52
saimon-dahal <saimondahal66@gmail.com>
2026-01-24 11:11:17 +1100

    chore: fetch colors from settings and refactor

--- ui/status_bar.py
+++ ui/status_bar.py
@@ -1,20 +1,35 @@
 import tkinter as tk
+from config.config import settings
+
 
 class StatusBar:
     """Creates and manages the status bar"""
+
     def __init__(self, parent):
         self.parent = parent
         self.create_status_bar()
-    
+
     def create_status_bar(self):
-        status_bar = tk.Frame(self.parent, bg="#e8e8e8", height=25)
+        status_bar = tk.Frame(
+            self.parent, bg=settings.colors.status_bar_background, height=25
+        )
         status_bar.grid(row=2, column=0, sticky="ew")
         status_bar.grid_propagate(False)
-        
-        self.status_label = tk.Label(status_bar, text="", bg="#e8e8e8", 
-                                     anchor=tk.W, padx=10, fg="#000000")
+
+        self.status_label = tk.Label(
+            status_bar,
+            text="",
+            anchor=tk.W,
+            padx=10,
+            bg=settings.colors.status_bar_background,
+            fg=settings.colors.status_bar_foreground,
+        )
         self.status_label.pack(fill=tk.BOTH)
-    
+
     def update(self, text):
         """Update status bar text"""
         self.status_label.config(text=text)
+        
+    def clear(self):
+        """Clear status bar text"""
+        self.status_label.config(text="")
