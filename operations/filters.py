4e9e04350b02befc3a7a68f4efab042378bed00b
saimon-dahal <saimondahal66@gmail.com>
2026-01-24 15:32:40 +1100

    add: placeholder functions

---
+++ operations/filters.py
@@ -0,0 +1,13 @@
+
+
+
+def apply_grayscale(image):
+    return image
+
+
+def apply_blur(image):
+    return image
+
+
+def apply_edge_detection(image):
+    return image
---
+++ operations/transforms.py
@@ -0,0 +1,11 @@
+
+def rotate_image(image):
+    return image
+
+
+def flip_image(image):
+    return image
+
+
+def resize_image(image):
+    return image
